4e671ff1f9dbaa5e0576c366cbe4386e1b76086d
Luke Babarinde <neofinder@gmail.com>
2025-06-25 12:57:07 -0400

    modified Dockerfile again

--- logger/logger_service.py
+++ logger/logger_service.py
@@ -68,7 +68,7 @@ def get_logs():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 # logger/logger_service.py
